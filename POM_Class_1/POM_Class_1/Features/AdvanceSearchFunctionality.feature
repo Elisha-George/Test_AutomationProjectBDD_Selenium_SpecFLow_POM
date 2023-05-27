00f70633b9be5fa1bc1431226ec51b2f9ff0033a
Elisha <elishageorge111@gmail.com>
2023-05-27 22:28:21 +0500

    Mid of Advance search pageClass

--- POM_Class_1/POM_Class_1/Features/AdvanceSearchFunctionality.feature
+++ POM_Class_1/POM_Class_1/Features/AdvanceSearchFunctionality.feature
@@ -7,21 +7,22 @@ Scenario: validate the advance search with complete data
 	Given click on advance search and navigate to it
 	And all the the deatils are filled 
 	When click on the search button
-	Then validate the product
+	Then validate the product success
+
 
 @tag1
 Scenario: validate the advance search by name only
 	Given click on advance search and navigate to it
 	And the name of the product is filled
 	When click on the search button
-	Then validate the product
+	Then validate the product success
 
-		@tag1
+@tag1
 Scenario: validate the advance search with invalid price range
 	Given click on advance search and navigate to it
 	And the price field is filled with alphabets
 	When click on the search button
-	Then validate the product
+	Then check the invlid field error 
 
 	@tag1
 Scenario: validate the advance search without any data
--- POM_Class_1/POM_Class_1/PageClass/AdvanceSearch.cs
+++ POM_Class_1/POM_Class_1/PageClass/AdvanceSearch.cs
@@ -10,7 +10,6 @@ using OpenQA.Selenium.Support.UI;
 using System.Threading;
 
 
-
 namespace POM_Class_1.PageClass 
 {
     internal class AdvanceSearch: Baseclass
@@ -21,6 +20,75 @@ namespace POM_Class_1.PageClass
             Description: Hemp, Spandex, Organic Cotton
             Short Description: Tee
             Price: 10 - 30*/
+
+
+        //Variables: 
+
+        string product_name = "";
+        string sku = "";
+        string description = "";
+        string short_description = "";
+        int price_1 = 10;
+        int price_2 = 30;
+
+        string invalidPrice_1 = "abcd";
+        string invalidPrice_2 = "abcd";
+
+
+        //Locators
+        By lnk_advanceSearch = By.XPath("");
+
+        By ProductName = By.XPath("");
+        By SKU = By.XPath("");
+        By Desc = By.XPath("");
+        By ShortDesc = By.XPath("");
+        By FirstPrice = By.XPath("");
+        By SecondPrice = By.XPath("");
+
+        By EmptyError = By.XPath("//body[1]/div[1]/main[1]/div[2]/div[2]/div[1]/div[1]/div[1]");
+        By SuccessMessage = By.XPath("");
+        By InvalidFieldError = By.XPath("//body[1]/div[1]/main[1]/div[3]/div[1]/form[1]/fieldset[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]");
+
+
+        //Methods
+
+        //Please enter a valid number.
+        //Enter a search term and try again.
+
+        public void navigateToAdvanceSearch()
+        {
+
+        }
+        public void FileFormDetails()
+        {
+
+        }
+        public void ClickSearch()
+        {
+
+        }
+        public void ValidateSuccess()
+        {
+
+        }
+        public void InvliadPrice()
+        {
+
+        }
+        public void FillNameOnly()
+        {
+
+        }
+        public void CheckEmptyError()
+        {
+
+        }
+        public void CheckPriceError()
+        {
+
+        }
+
+
 
     }
 }
